ac6c51690ad61d60b5185d459cc9e24f1611d842
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-04-20 21:46:22 -0700

    displaying the items after the product for sale has been added

--- bamazon.js
+++ bamazon.js
@@ -1,5 +1,6 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+
 var connection = mysql.createConnection({
     host:"localhost",
 	port:3306,
@@ -12,12 +13,12 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  returnProducts();
+  // returnProducts();
+  start();
 });
 
 function start() {
-  inquirer
-    .prompt({
+  inquirer.prompt({
       name: "userType",
       type: "list",
       message: "Please choose the best fit scenario below?",
@@ -27,6 +28,7 @@ function start() {
       // based on their answer, either call the bid or the post functions
       if (answer.userType === "I want to buy on Bamazon") {
         console.log("get ready to buy!");
+        returnProducts();
         purchaseItem();//TODO
       }else if(answer.userType === "I am a Bamazon Seller"){
         console.log("get ready to sell!")
@@ -45,28 +47,28 @@ function returnProducts() {
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
     console.log(res);
-    insertProduct();
+    // insertProduct();
     // start();
-    connection.end();
+    
   });
 };
 
 //Insert a product will be called as a part of prompt
-function insertProduct(){
-  var postItem = {
-    product_name:"All Star basketball shorts for women",
-    department_name:"Sports",
-    list_price_per: 25.99,
-    on_sale_price: 20.99,
-    Inventory:30
-  };
-  let query = connection.query('INSERT INTO products SET ?', postItem, function (error, res, fields) {
-    if (error) throw error;
-    console.log(res);
-  // Neat!
-  });
-console.log(query.sql); 
-};
+// function insertProduct(){
+//   var postItem = {
+//     product_name:"All Star basketball shorts for women",
+//     department_name:"Sports",
+//     list_price_per: 25.99,
+//     on_sale_price: 20.99,
+//     Inventory:30
+//   };
+//   let query = connection.query('INSERT INTO products SET ?', postItem, function (error, res, fields) {
+//     if (error) throw error;
+//     console.log(res);
+//   // Neat!
+//   });
+// console.log(query.sql); 
+// };
 
 function sellItem() {
   // prompt for info about the item being put up for auction
@@ -82,17 +84,9 @@ function sellItem() {
         type: "list",
         message: "In which category would you like to place your product for sale?",
         choices: [
-          "Apparel", 
-          "Camera", 
-          "Electronics",
-          "Health and Beauty",
-          "Kitchen",
-          "Misc.",
-          "Personal Computers",
-          "Sports",
-          "Tools & Home Improvement",
-          "Toy",
-          "Wireless Phone Accessory"
+          "Apparel", "Camera", "Electronics","Health and Beauty","Kitchen",
+          "Misc.", "Personal Computers", "Sports","Tools & Home Improvement",
+          "Toy","Wireless Phone Accessory"
         ]
       },
       {
@@ -100,7 +94,7 @@ function sellItem() {
         type: "input",
         message: "What would you like your list price to be?",
         validate: function(value) {
-          if ($.isNumeric(value) === true) {
+          if (Math.round(value)) {
             return true;
           }
           return false;
@@ -111,10 +105,9 @@ function sellItem() {
       type: "input",
       message: "Please enter your on sale price. If not on sale enter list price?",
       validate: function(value) {
-          if ($.isNumeric(value) === true) {
+          if (Math.round(value)) {
             return true;
-          }
-          return false;
+          } else return false;
         }
       },
       {
@@ -122,35 +115,49 @@ function sellItem() {
       type: "input",
       message: "Please enter your the amount of the product you have in inventory to sell.",
       validate: function(value) {
-          if ($.isNumeric(value) === true) {
+          if (Math.round(value)) {
             return true;
-          }
-          return false;
+          }else return false;
         }
       },
 
     ])
     .then(function(answer) {
       // when finished prompting, insert a new item into the db with that info
-     insertProduct();
-    });
+      var postItem = {
+        product_name:answer.product_name,
+        department_name:answer.department_name,
+        list_price_per: answer.list_price_per,
+        on_sale_price: answer.on_sale_price,
+        Inventory:answer.inventory
+      };
+      let query = connection.query('INSERT INTO products SET ?', postItem, function (error, res, fields) {
+        if (error) throw error;
+        console.log(res);
+       
+      // Neat!
+      });
+    console.log(query.sql); 
+    returnProducts();
+    connection.end();//Adding the end connection her so the it stops when the query is done
+  });
 }
 
-function insertProduct(){
-  var postItem = {
-    product_name:answer.product_name,
-    department_name:answer.department_name,
-    list_price_per: answer.list_price_per,
-    on_sale_price: answer.on_sale_price,
-    Inventory:answer.inventory
-  };
-  let query = connection.query('INSERT INTO products SET ?', postItem, function (error, res, fields) {
-    if (error) throw error;
-    console.log(res);
-  // Neat!
-  });
-console.log(query.sql); 
-};
+// function insertProduct(answer){
+//   var postItem = {
+//     product_name:answer.product_name,
+//     department_name:answer.department_name,
+//     list_price_per: answer.list_price_per,
+//     on_sale_price: answer.on_sale_price,
+//     Inventory:answer.inventory
+//   };
+//   let query = connection.query('INSERT INTO products SET ?', postItem, function (error, res, fields) {
+//     if (error) throw error;
+//     console.log(res);
+//   // Neat!
+//   });
+// console.log(query.sql); 
+// };
 // function start(){
 // 	console.log("The program is ready to accept user inputs from inquirer!");
 	
